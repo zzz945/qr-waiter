114f3ff9f854410d1c4089b47a4969297ec7bdcc
zzz945 <642010820@qq.com>
2016-09-11 21:07:02 +0800

    初级美工

---
+++ src/vuex/actions.js
@@ -0,0 +1,4 @@
+export const setTableId = ({ dispatch }, id) => dispatch('UPDATE_TABLEID', id)
+export const setStatus = ({ dispatch }, status) => dispatch('UPDATE_STATUS', status)
+export const addFood = ({ dispatch }, food) => dispatch('ADD_FOOD', food)
+export const removeFood = ({ dispatch }, food) => dispatch('REMOVE_FOOD', food)
