151b0f4f3b94c187276f9b80d3d9275d7c17ffb2
zzz945 <642010820@qq.com>
2016-09-15 23:01:06 +0800

    数据库读取订单列表并显示出来了

--- src/Home.vue
+++ src/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div style="width=100%;">
-    <group title="isLink is set to true when link exists">
-      <cell title="Go to Radio" link="/component/radio" inline-desc='link="/component/radio"'></cell>
-      <cell title="Go to Demo" :link="{path:'/demo'}" inline-desc=':link={path:"/demo"}'></cell>
-      <cell title="http link" link="https://vux.li" inline-desc='link="https://vux.li"'></cell>
+    <group title="有新订单了" v-for="order in order_list">
+      <cell :title="order.r_id" link="/component/radio" :inline-desc='order.t_id'></cell>
     </group>
   </div>
 </template>
@@ -11,22 +9,55 @@
 
 import { Group, Cell } from './components'
 
+const myarray = [
+  {
+    openid: 'oRcn4wF1K1KZc_eELqveTXpKTNuQ',
+    r_id: '11'
+  }
+]
+
 export default {
   components: {
     Group,
     Cell
   },
+  data () {
+    return {
+      myarray: myarray,
+      order_list: []
+    }
+  },
   ready () {
-    // let openid = document.getElementById('arg_openid').innerHTML
-    let params = {
-      r_id: '11'
+    let openid = document.getElementById('arg_openid').innerHTML
+    let rId
+    for (let i = 0; i < this.myarray.length; i++) {
+      if (openid === myarray[i].openid) {
+        rId = myarray[i].r_id
+      }
     }
 
+    let params = {
+      r_id: rId
+    }
+
+    let _this = this
     setTimeout(function () {
-      this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrorder/hasNewOrder', {params: params}).then((response) => {
+      _this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrorder/hasNewOrder', {params: params}).then((response) => {
         if (response.data === 'yes') {
-          this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrorder/getNewOrder', {params: params}).then((response) => {
-            console.log('getNewOrder:' + response.data)
+          _this.$http.get('http://tdkjgzh.applinzi.com/Home/Qrorder/getNewOrder', {params: params}).then((response) => {
+            console.log('getNewOrder:')
+            console.log(response.data)
+            let strList = JSON.parse(response.data)
+            for (let i = 0; i < strList.length; i++) {
+              let order = {}
+              order.openid = strList[i].openid
+              order.r_id = strList[i].r_id
+              order.t_id = strList[i].t_id
+              order.food_list = JSON.parse(strList[i].food_list)
+              _this.order_list.push(order)
+            }
+            console.log('this.order_list:')
+            console.log(_this.order_list)
           }, (response) => {
             console.log('getNewOrder failed:')
             console.log(response)
@@ -38,7 +69,7 @@ export default {
         console.log('hasNewOrder failed:')
         console.log(response)
       })
-    }, 10000)
+    }, 1000)
   }
 }
 </script>
