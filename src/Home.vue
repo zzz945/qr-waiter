07e0af0d342486b4292e581d17305939f14ad6a2
zzz945 <642010820@qq.com>
2016-09-19 12:52:50 +0800

    消息发送与存储功能实现

--- src/Home.vue
+++ src/Home.vue
@@ -1,28 +1,145 @@
 <template>
   <div style="width=100%;">
+    <divider v-show="this.tId === ''">未绑定桌牌号</divider>
+    <divider v-show="this.tId !== ''">{{tId}}号桌</divider>
     <div>
-      <div class="round-btn">
-        <x-button class="round-btn-class" type="primary" @click="scanTableId">加水</x-button>
-      </div>
-      <div class="round-btn">
-        <x-button class="round-btn-class" type="primary" @click="scanTableId">加餐</x-button>
-      </div>
-      <div class="round-btn">
-        <x-button class="round-btn-class" type="primary" @click="scanTableId">买单</x-button>
-      </div>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('茶水')">茶水</div>
+      </a>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('酒水')">酒水</div>
+      </a>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('点菜')">点菜</div>
+      </a>
+    </div>
+    <div>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('餐具')">餐具</div>
+      </a>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('空调')">空调</div>
+      </a>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('热菜')">热菜</div>
+      </a>
+    </div>
+    <div>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('火锅加汤')">火锅加汤</div>
+      </a>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('烧烤换纸')">烧烤换纸</div>
+      </a>
+      <a class="round-btn-box">
+        <div class="round-btn" v-on:click="sendMsg('叫服务员')">叫服务员</div>
+      </a>
     </div>
   </div>
 </template>
 <script>
-import { XButton } from './components'
+import { Divider } from './components'
+import wx from 'we-jssdk'
 
 export default {
   components: {
-    XButton
+    Divider
+  },
+  data () {
+    return {
+      tId: '',
+      rId: '',
+      msg: ''
+    }
   },
   methods: {
+    sendMsg (msg) {
+      console.log('sendMsg:' + msg)
+      this.msg = msg
+      if (this.tId === '') {
+        this.scanTableId()
+      } else {
+        this._sendMsg(this)
+      }
+    },
+    _sendMsg (_this) {
+      let params = {
+        r_id: _this.rId,
+        t_id: _this.tId,
+        msg: _this.msg
+      }
+      _this.$http.get('http://tdkjgzh.applinzi.com/home/qrwaiter/addMsg', {params: params}).then((response) => {
+        _this.$vux.alert.show({
+          title: '提示',
+          content: '您的消息已发送到前台，请稍等',
+          onHide () {
+          }
+        })
+        console.log('addMsg:' + response.data)
+      }, (response) => {
+        console.log('addMsg failed')
+        console.log(params)
+        console.log(response)
+      })
+    },
+    scanTableId () {
+      let _this = this
+      _this.$vux.alert.show({
+        title: '提示',
+        content: '请扫描桌角二维码',
+        onHide () {
+          _this._scanTableId(_this)
+        }
+      })
+    },
+    _scanTableId (_this) {
+      wx.scanQRCode({
+        needResult:	1,
+        scanType: ['qrCode', 'barCode'],
+        success: (res) => {
+          try {
+            var result = JSON.parse(res.resultStr)
+            if (result.TABLE_ID === undefined) {
+              throw new Error('TABLE_ID is undefined')
+            }
+            _this.tId = result.TABLE_ID
+            _this.rId = result.RESTAURANT_ID
+            _this._sendMsg(_this)
+          } catch (e) {
+            console.log('_scanTableId:' + e)
+            _this.$vux.alert.show({
+              title: '提示',
+              content: '餐桌二维码错误，请扫描桌角二维码',
+              onHide () {
+                _this._scanTableId(_this)
+              }
+            })
+          }
+        }
+      })
+    }
   },
   ready () {
+    // GET /someUrl
+    this.$http.get('http://tdkjgzh.applinzi.com/home/qrwaiter/getSignPackage').then((response) => {
+      var result = JSON.parse(response.data)
+      console.log('getSignPackage:' + response.data)
+
+      wx.config({
+        debug:	false,
+        appId:	result.appId,
+        timestamp:	result.timestamp,
+        nonceStr:	result.nonceStr,
+        signature:	result.signature,
+        jsApiList:	['scanQRCode']
+      })
+
+      wx.ready(() => {
+        console.log('wx.ready')
+      })
+    }, (response) => {
+      console.log('get http://tdkjgzh.applinzi.com/home/qrwaiter/getSignPackage failed')
+    })
   }
 }
 </script>
@@ -30,17 +147,18 @@ export default {
 <style lang="less" scoped>
   @import './styles/index.less';
   @import './styles/variable.less';
-  .round-btn {
-    width: 30vmin;
-    height: 30vmin;
-    display: inline-block;
-    .round-btn-class {
+  .round-btn-box {
+    float: left;
+    .round-btn {
       border-radius: 100%;
       width: 100%;
       height: 100%;
-      &:after {
-        border-radius: 100%;
-      }
+      width: 24vmin;
+      height: 24vmin;
+      background-color: @theme-color;
+      .vux-center;
+      margin-left: 7vmin;
+      margin-top: 7vmin;
     }
   }
 </style>
